14c7b36293087a0555556f291643fe229c7a0a17
Eli Davidson <eli@shadowlab.dev>
2022-04-21 06:22:29 -0600

    Display dashboard for any user

--- src/scripts.js
+++ src/scripts.js
@@ -6,6 +6,21 @@ import './css/styles.css';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
-import { getAllRooms, getCustomerByID, getAllCustomers, getAllBookings, addNewBooking, deleteBooking } from './classes/api-endpoints'
+import { getAllRooms, getCustomerByID, getAllCustomers, getAllBookings, getBookingsById, addNewBooking, deleteBooking } from './classes/api-endpoints'
+import { displayBookings, displayUsername } from './classes/domUpdates';
+import User from './classes/User';
 
-console.log();
+
+const currentUserID = 1
+
+getCustomerByID(currentUserID).then(user => displayUsername(user))
+getBookingsById(currentUserID).then(bookings => displayBookings(bookings))
+
+
+
+
+
+
+
+
+
