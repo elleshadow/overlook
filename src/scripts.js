5c2d6661ef863349f9daa32470363c7c3d07ad6d
Eli Davidson <eli@shadowlab.dev>
2022-04-26 19:01:09 -0600

    Update scripts file

--- src/scripts.js
+++ src/scripts.js
@@ -1,26 +1,29 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
+import "./css/styles.css";
+import { getCustomerByID } from "./classes/api-endpoints";
+import {
+  displayBookingsByUserID,
+  displayUsername,
+  setCalendarLimits,
+} from "./classes/domUpdates";
 
-// An example of how you tell webpack to use a CSS (SCSS) file
-import './css/styles.css';
+//Temp Functions==========================================================
 
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
-import { getAllRooms, getCustomerByID, getAllCustomers, getAllBookings, getBookingsById, addNewBooking, deleteBooking } from './classes/api-endpoints'
-import { displayBookings, displayUsername } from './classes/domUpdates';
-import User from './classes/User';
+let randomUserBtn = document.querySelector(".random_user_button");
+randomUserBtn.addEventListener("click", () => run());
 
+const getRandomUser = () => {
+  const maxUsers = 50;
+  return Math.floor(Math.random() * maxUsers);
+};
 
-const currentUserID = 1
+//===================================================================
 
-getCustomerByID(currentUserID).then(user => displayUsername(user))
-getBookingsById(currentUserID).then(bookings => displayBookings(bookings))
+const run = (userID = getRandomUser()) => {
+  setCalendarLimits();
+  getCustomerByID(userID)
+    .then((user) => displayUsername(user))
+    .catch(() => console.log(userID));
+  displayBookingsByUserID(userID);
+};
 
-
-
-
-
-
-
-
-
+run();
